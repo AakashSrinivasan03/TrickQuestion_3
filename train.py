a41ad17867e18a31831af3b60bc148587bc475f6
AakashSrinivasan03 <34432149+AakashSrinivasan03@users.noreply.github.com>
2018-03-17 16:51:02 +0530

    Add val accuracy

--- train.py
+++ train.py
@@ -14,6 +14,7 @@ import scipy.special
 import sklearn
 import sklearn.metrics
 import sklearn.preprocessing
+import tensorflow as tf
 def main():
 	global args 
 	args = parse_arguments()
@@ -46,6 +47,10 @@ def main():
 	m_val  = val_X.shape[0]
 	features = 784
 	classes = 10
+	MAX_EPOCHS = 25
+	alpha = 0.0001
+	gamma = 0.001
+	eps = 0.000001
 
 
 	# One hot encoded form
@@ -63,10 +68,11 @@ def main():
 	np.random.shuffle(train)
 	np.random.shuffle(val)
 
-	train_X = train[:,0:features].reshape(28,28)
+	train_X = train[:,0:features].reshape(train.shape[0],28,28)
 	train_Y = train[:,features:]
-	val_X  = val[:,0:features].reshape(28,28)
+	val_X  = val[:,0:features].reshape(val.shape[0],28,28)
 	val_Y  = val[:,features:]
+	print(train_X.shape)
 	#scalar=sklearn.preprocessing.StandardScaler()
 	#scalar=scalar.fit(train_X)
 	#train_X=scalar.transform(train_X)
@@ -79,41 +85,26 @@ def main():
 
 	test_prediction=[]
 	
-	if (args.pretrain):
+	'''if (args.pretrain):
 		with open('model/W.pickle', 'rb') as handle:
 			W_dict = pickle.load(handle)
 		with open('model/b.pickle', 'rb') as handle:
     			b_dict = pickle.load(handle)
 	else:
 		W_dict,b_dict=minibatch_gradient_descent(W_dict,b_dict,args.num_hidden,train_X,train_Y,args.batch_size,val_X,val_Y)		
+'''
 
 
 
 
-
-	sub.to_csv("sub_30.csv", index=False)
-	with open(model_dir+'/W.pickle', 'wb') as handle:
+	#sub.to_csv("sub_30.csv", index=False)
+	'''with open(model_dir+'/W.pickle', 'wb') as handle:
 		pickle.dump(W_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)	
 	with open(model_dir+'/b.pickle', 'wb') as handle:
-		pickle.dump(b_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
-model()
-sess=tf.Session()
-for i in range(0,int(train_X.shape[2]/args.batch_size)):
-	X_batch=train_X[:,:,i*args.batch_size:np.max((i+1)*args.batch_size,55000)]
-	y_batch=train_X[i*args.batch_size:np.max((i+1)*args.batch_size,55000)]
-	sess.run()
-
-
-	
-MAX_EPOCHS = 25
-alpha = 0.0001
-gamma = 0.001
-eps = 0.000001
-
-def model():
-	X=tf.placeholder(tf.float32)
-	y=tf.placeholder(tf.float32)
-	conv1 = tf.layers.conv2d(X, 64, 3, 1, 'same', activation=tf.nn.relu)    
+		pickle.dump(b_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)'''
+	X=tf.placeholder(tf.float32,shape=(None, 28,28,1))
+	y=tf.placeholder(tf.float32,shape=(None))
+	conv1 = tf.layers.conv2d(X, 64, 3, (1, 1), 'same', activation=tf.nn.relu)    
 	pool1 = tf.layers.max_pooling2d(conv1, 2, 2) 
 
 	conv2 = tf.layers.conv2d(pool1, 128, 3, 1, 'same', activation=tf.nn.relu)    
@@ -122,15 +113,16 @@ def model():
 	conv3 = tf.layers.conv2d(pool2, 256, 3, 1, 'same', activation=tf.nn.relu)    
 	conv4 = tf.layers.conv2d(conv3, 256, 3, 1, 'same', activation=tf.nn.relu)    
 
+
 	pool3 = tf.layers.max_pooling2d(conv4, 2, 2) 
+	op=tf.shape(pool3)
 
-
-	flat = tf.reshape(pool2, [-1, 7*7*32])      
+	flat = tf.reshape(pool3, [tf.shape(X)[0],256*3*3 ])      
 	fc1 = tf.layers.dense(flat, 1024)
 	fc2 = tf.layers.dense(fc1, 1024)
 
 	fc3 = tf.layers.dense(fc2, 10)
-	tf.nn.batch_normalization(fc3, )
+	#tf.nn.batch_normalization(fc3, )
 	y_pred = tf.nn.softmax(fc3)
 
 
@@ -138,16 +130,37 @@ def model():
 
 
 	cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=y_pred,
-	                                                        labels=y_true)
+	                                                        labels=y)
 	loss = tf.reduce_mean(cross_entropy)
-	optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
-	correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+	optimizer = tf.train.AdamOptimizer(learning_rate=0.1).minimize(loss)
+	correct_prediction = tf.equal(y_pred, y)
 	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 
 
 	#session = tf.Session()
-	train_batch_size = args.batch_size
+	#train_batch_size = args.batch_size
+	#args.batch_size=100
+	sess=tf.Session()
+	sess.run(tf.global_variables_initializer())
+	for epochs in range(0,1):
+		for i in range(0,int(train_X.shape[0]/args.batch_size)):
+			X_batch=train_X[i*args.batch_size:np.minimum((i+1)*args.batch_size,55000),:,:]
+			y_batch=train_Y[i*args.batch_size:np.minimum((i+1)*args.batch_size,55000)]
+			_,val=sess.run([optimizer,accuracy],feed_dict={X:np.expand_dims(X_batch,3),y:y_batch})
+			print(val)
+			print(sess.run(accuracy,feed_dict={X:np.expand_dims(val_X,3),y:val_Y}))
+			
+
+
+
+		
+	MAX_EPOCHS = 25
+	alpha = 0.0001
+	gamma = 0.001
+	eps = 0.000001
+
+
 
 
 
@@ -166,13 +179,13 @@ def parse_arguments():
 	parser.add_argument("--activation",default='sigmoid',type=str, help="the choice of activation function - valid values are tanh/sigmoid")
 	parser.add_argument("--loss",default='ce' ,type=str, help="possible choices are squared error[sq] or cross entropy loss[ce]")
 	parser.add_argument("--opt", default='adam',type=str, help="the optimization algorithm to be used: gd, momentum, nag, adam - you will be implementing the mini-batch version of these algorithms")
-	parser.add_argument("--batch_size",default=20 ,type=int, help="the batch size to be used - valid values are 1 and multiples of 5")
+	parser.add_argument("--batch_size",default=10000 ,type=int, help="the batch size to be used - valid values are 1 and multiples of 5")
 	parser.add_argument("--anneal" ,default=True,type=bool,help="if true the algorithm should halve the learning rate if at any epoch the validation loss decreases and then restart that epoch")
 	parser.add_argument("--save_dir",default='Save_Dir',type=str, help="the directory in which the pickled model should be saved - by model we mean all the weights and biases of the network")
 	parser.add_argument("--expt_dir",default='Expt_Dir' ,type=str, help= "the directory in which the log files will be saved - see below for a detailed description of which log files should be generated")
-	parser.add_argument("--train",default="fashionmnist/train.csv",type=str, help="path to the Training dataset")
-	parser.add_argument("--test",default="fashionmnist/test.csv" ,type=str, help = "path to the Test dataset")
-	parser.add_argument("--val",default="fashionmnist/val.csv" ,type=str, help = "path to the val dataset")
+	parser.add_argument("--train",default="../fashionmnist/train.csv",type=str, help="path to the Training dataset")
+	parser.add_argument("--test",default="../fashionmnist/test.csv" ,type=str, help = "path to the Test dataset")
+	parser.add_argument("--val",default="../fashionmnist/val.csv" ,type=str, help = "path to the val dataset")
 	parser.add_argument("--pretrain",default=False ,type=str, help ="pretrain")
 	args = parser.parse_args()
 	return args
